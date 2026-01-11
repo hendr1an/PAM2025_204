76bb27bb46ac9fe9d044b3428a9a7f257c5e03ca
hendr1an <hendriansyahgyan@gmail.com>
2026-01-12 01:24:23 +0700

    membuat file HomeScreen.kt

---
+++ app/src/main/java/com/example/crabsupply/ui/buyer/HomeScreen.kt
@@ -0,0 +1,32 @@
+package com.example.crabsupply.ui.buyer
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun HomeScreen(
+    onLogoutClick: () -> Unit = {}
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Selamat Datang di", fontSize = 20.sp)
+        Text(
+            text = "Crab Supply",
+            fontSize = 32.sp,
+            style = MaterialTheme.typography.headlineMedium,
+            color = MaterialTheme.colorScheme.primary
+        )
+        Spacer(modifier = Modifier.height(24.dp))
+        Button(onClick = onLogoutClick) {
+            Text("LOGOUT / KELUAR")
+        }
+    }
+}
