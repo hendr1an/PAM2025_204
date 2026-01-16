eb896ae31fbf059f0b7137222eeb9b9deb343959
hendr1an <hendriansyahgyan@gmail.com>
2026-01-16 18:25:44 +0700

    Update OrderRepository.kt agar tahu cara mengambil daftar pesanan dan mengupdate statusnya.

--- app/src/main/java/com/example/crabsupply/data/repository/OrderRepository.kt
+++ app/src/main/java/com/example/crabsupply/data/repository/OrderRepository.kt
@@ -2,6 +2,9 @@ package com.example.crabsupply.data.repository
 
 import com.example.crabsupply.data.model.Order
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
 
 class OrderRepository {
     private val firestore = FirebaseFirestore.getInstance()
@@ -18,5 +21,31 @@ class OrderRepository {
             .addOnFailureListener { e ->
                 onResult(false, "Gagal memesan: ${e.message}")
             }
+    }
+
+    fun getAllOrdersRealtime(): Flow<List<Order>> = callbackFlow {
+        val listener = firestore.collection("orders")
+            .orderBy("timestamp", com.google.firebase.firestore.Query.Direction.DESCENDING) // Urutkan dari yang terbaru
+            .addSnapshotListener { snapshot, error ->
+                if (error != null) {
+                    close(error)
+                    return@addSnapshotListener
+                }
+                if (snapshot != null) {
+                    val orders = snapshot.documents.map { doc ->
+                        doc.toObject(Order::class.java)!!.copy(id = doc.id)
+                    }
+                    trySend(orders)
+                }
+            }
+        awaitClose { listener.remove() }
+    }
+
+    // 2. FUNGSI UPDATE STATUS PESANAN
+    fun updateOrderStatus(orderId: String, newStatus: String, onResult: (Boolean) -> Unit) {
+        firestore.collection("orders").document(orderId)
+            .update("status", newStatus)
+            .addOnSuccessListener { onResult(true) }
+            .addOnFailureListener { onResult(false) }
     }
 }
